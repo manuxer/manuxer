b7c9bf8ea0f482fa9fd620efbd706486e3108b5c
manuxer <manuxdesign@gmail.com>
2025-11-12 00:19:20 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,5 @@
-// import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
-// import './index.css'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// createRoot(document.getElementById("root")!).render(<App />);
-
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/manuxer">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
