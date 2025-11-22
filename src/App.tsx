165f7d270a23d95dd98c044dfad21a439c93bf16
manuxer <manuxdesign@gmail.com>
2025-11-22 14:28:17 -0500

    removing password temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "@/context/AuthContext";
-import ProtectedRoute from "@/components/ProtectedRoute";
-import Login from "./pages/Login";
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -18,22 +15,61 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ProtectedRoute><Index /></ProtectedRoute>} />
-          <Route path="/projects" element={<ProtectedRoute><Projects /></ProtectedRoute>} />
-          <Route path="/projects/:id" element={<ProtectedRoute><ProjectDetail /></ProtectedRoute>} />
-          <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
-          <Route path="/resume" element={<ProtectedRoute><Resume /></ProtectedRoute>} />
+          <Route path="/" element={<Index />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </TooltipProvider>
-    </AuthProvider>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
+
+
+
+
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { AuthProvider } from "@/context/AuthContext";
+// import ProtectedRoute from "@/components/ProtectedRoute";
+// import Login from "./pages/Login";
+// import Index from "./pages/Index";
+// import Projects from "./pages/Projects";
+// import ProjectDetail from "./pages/ProjectDetail";
+// import About from "./pages/About";
+// import Resume from "./pages/Resume";
+// import NotFound from "./pages/NotFound";
+
+// const queryClient = new QueryClient();
+
+// const App = () => (
+//   <QueryClientProvider client={queryClient}>
+//     <AuthProvider>
+//       <TooltipProvider>
+//         <Toaster />
+//         <Sonner />
+//         <Routes>
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/" element={<ProtectedRoute><Index /></ProtectedRoute>} />
+//           <Route path="/projects" element={<ProtectedRoute><Projects /></ProtectedRoute>} />
+//           <Route path="/projects/:id" element={<ProtectedRoute><ProjectDetail /></ProtectedRoute>} />
+//           <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
+//           <Route path="/resume" element={<ProtectedRoute><Resume /></ProtectedRoute>} />
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </TooltipProvider>
+//     </AuthProvider>
+//   </QueryClientProvider>
+// );
+
+// export default App;
